345049d4ce9ee5ae36e9d002fd68c610fbe5c210
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-15 16:35:33 +0700

    Add option -Wall

--- PETSC/build.sh
+++ PETSC/build.sh
@@ -2,7 +2,7 @@
 basn=`basename $1 .c`
 
 # Using static lib
-mpicc $1 -I/usr/local/petsc-3.8.3_openmpi_gnu/include \
+mpicc -Wall $1 -I/usr/local/petsc-3.8.3_openmpi_gnu/include \
 /usr/local/petsc-3.8.3_openmpi_gnu/lib/libpetsc.a \
 -lblas -llapack -lm -lX11 -ldl -o $basn.x
 
