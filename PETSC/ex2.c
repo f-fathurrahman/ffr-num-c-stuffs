83887e3ccf089f7dadd7de1c3102662041dd22c5
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-15 16:40:54 +0700

    Test output of MatGetOwnershipRange in ex2.c

--- PETSC/ex2.c
+++ PETSC/ex2.c
@@ -1,4 +1,4 @@
-#include <petscsys.h>
+#include <petscksp.h>
 
 static char help[] = "Solves a linear system in parallel with KSP.\n\
 Input parameters include:\n\
@@ -11,19 +11,35 @@ Input parameters include:\n\
 int main( int argc, char** argv )
 {
   PetscErrorCode ierr;
-  Vec x, b, u;
 
-  ierr = PetscInitialize( &argc, &argv, (char*)0, help );
+  ierr = PetscInitialize( &argc, &argv, (char*)0, help ); CHKERRQ( ierr );
 
   // Problem size
   PetscInt m = 8, n = 7;
   ierr = PetscOptionsGetInt( NULL, NULL, "-m", &m, NULL );
   ierr = PetscOptionsGetInt( NULL, NULL, "-n", &n, NULL );
 
+  //
   // Matrix of linear system
+  //
   Mat A;
   ierr = MatCreate( PETSC_COMM_WORLD, &A );
   ierr = MatSetSizes( A, PETSC_DECIDE, PETSC_DECIDE, m*n, m*n );
   ierr = MatSetFromOptions( A );
+  ierr = MatMPIAIJSetPreallocation( A, 5, NULL, 5, NULL );
+  ierr = MatSeqAIJSetPreallocation( A, 5, NULL );
+  ierr = MatSeqSBAIJSetPreallocation( A, 1, 5, NULL );
+  ierr = MatMPISBAIJSetPreallocation( A, 1, 5, NULL, 5, NULL );
 
+  //
+  // Determined which rows of the matrix are locally owned
+  //
+  PetscInt Istart, Iend;
+  ierr = MatGetOwnershipRange( A, &Istart, &Iend );
+  printf("Istart = %d, Iend = %d\n", Istart, Iend );
+
+  ierr = PetscFinalize(); CHKERRQ( ierr );
+
+  printf("Program ended normally\n");
+  return 0;
 }
