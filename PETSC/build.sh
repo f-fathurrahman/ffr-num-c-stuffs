a2e1b34aea817a9820e17d48f09417a0ace1af5c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-02-14 14:32:20 +0700

    Fix mistake in linking to PETSC

--- PETSC/build.sh
+++ PETSC/build.sh
@@ -1,9 +1,10 @@
-export PETSC_DIR=/usr/local/petsc-3.8.3_openmpi_gnu/
+export PETSC_DIR=/usr/local/petsc-3.8.3_openmpi_gnu
 
-export INC_PETSC=$PETSC_DIR/include/
+export INC_PETSC=$PETSC_DIR/include
 
-export LIB_PETSC="-L$PETSC_DIR -lpetsc -lm"
+export LIB_PETSC="-L${PETSC_DIR}/lib -lpetsc -lm"
 
+echo $LIB_PETSC
 basnam=`basename $1 .c`
 mpicc -I$INC_PETSC $1 $LIB_PETSC -o $basnam.x
 
